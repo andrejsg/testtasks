e46c025cde9a41a6ab987dc773f573ebb70a8668
Andrejs Griscuks <andrejsgriscuks@gmail.com>
2014-12-05 22:47:27 +0200

    Add rss

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,11 @@
 #encoding: utf-8
 
+require 'rss'
+
 class StaticPagesController < ApplicationController
   
   def index
+  	@rss = @rss = RSS::Parser.parse(open('http://www.tvnet.lv/rss/frontpage.xml').read, false).items[0..4]
   end
   
 end
